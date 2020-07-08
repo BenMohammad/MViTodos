34947a6e17ed32c296adeb1b1f46de5e9da7f8c5
Ben Mohammad <benmohammad593@gmail.com>
2020-07-08 22:01:24 +0100

    TasksFragment added

--- app/src/main/java/com/benmohammad/mvitodos/taskdetail/TaskDetailFragment.kt
+++ app/src/main/java/com/benmohammad/mvitodos/taskdetail/TaskDetailFragment.kt
@@ -72,6 +72,7 @@ class TaskDetailFragment: Fragment(), MviView<TaskDetailIntent, TaskDetailViewSt
     override fun onDestroy() {
         super.onDestroy()
         disposables.dispose()
+        
     }
 
 
